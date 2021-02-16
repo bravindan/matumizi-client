4a2feb1475995b2747b1695f8996770044e9d984
bravindan <danielbravin0@gmail.com>
2021-02-16 17:41:06 +0300

    Created component

---
+++ src/app/matumizi/matumizi.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-matumizi',
+  templateUrl: './matumizi.component.html',
+  styleUrls: ['./matumizi.component.css']
+})
+export class MatumiziComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
